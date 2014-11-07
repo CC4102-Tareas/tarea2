496b995363d389fd818ea868c48fab42c6e2402b
Felipe Hernández <cephei.1313@gmail.com>
2014-11-06 21:54:45 -0300

    ahora funciona todo conchetuamregit statusgit status

--- Arbol-Optimo/arbol-optimo.c
+++ Arbol-Optimo/arbol-optimo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define DEBUG 1
+
+#define DEBUG 0
 
 /**
   Implementación hecha según Cormen (capítulo 15, página 397)
@@ -21,8 +22,9 @@ Nodo *crear_nodo(int);
 void insertar(Nodo **, Nodo **);
 Nodo *buscar(Nodo **, int);
 
-void generar_tablas(float[], float[], int);
-void imprimir_matrices();
+void generar_tablas(float*, float*, int, float**, float**, int**);
+Nodo* crear_arbol_optimo(int *, int **, int, int);
+void imprimir_matrices(float **, float **, int **, int);
 
 void dibujar_arbol(Nodo *);
 void dibujar_arbol_helper(Nodo *, int);
@@ -39,49 +41,46 @@ void mostrar_nodo(Nodo *);
   @param argv[3] = Path del archivo que contiene el arreglo con las llaves.
   @param argv[4] = Path del archivo que contiene el arreglo con las busquedas a ser realizadas.
  */
-void main(int argc, char *argv[]); {
+void main(int argc, char *argv[]) {
 
     // Contador
     int i;
-
+/*
     // Arreglo para cargar los elementos (4 MB)
     int *llaves = malloc(1024 * 1024 * sizeof(int));
 
-    // Arreglo para cargar los elementos (4 MB)
+    // Arreglo para cargar las prob de los elementos (4 MB)
     float *prob = malloc(1024 * 1024 * sizeof(int));
-    
+
     // Arreglo para cargar consultas al arbol (buffer de 400 kB)
     int *buffer_consultas = malloc(1024 * 1024 * 100 * sizeof(int));
 
     // Exponente
     int exp = atoi(argv[1]);
-
+*/
     // Variables relativas al tiempo de ejecucion
     struct timeval antes , despues;
     double tiempo_construccion, tiempo_busqueda;
 
-    // Variables para construir y consultar arbol
-    Nodo *nuevo_nodo, *raiz, *tmp;
+    // Variables para construir y consultar arbol. Solo necesitamos la raíz.
+    Nodo *raiz;
     raiz = NULL;
-
+/*
     // Lecturas de archivo: inserciones y consultas
-
     FILE * fptr = fopen(argv[2], "rb");
-
     fread(llaves, 1024 * 1024 * sizeof(int), 1, fptr);
-
     fclose(fptr);
 
     fptr = fopen(argv[3], "rb");
-
-    fread(buffer_consultas, 1024 * 1024 * 100 * sizeof(int), 1, fptr);
-
+    fread(prob, 1024 * 1024 * 100 * sizeof(int), 1, fptr);
     fclose(fptr);
 
-
-    int i;
-    int N = argv[4];
-    int ctd_elem_cons = argv[100*argv[1]];
+    fptr = fopen(argv[4], "rb");
+    fread(buffer_consultas, 1024 * 1024 * 100 * sizeof(int), 1, fptr);
+    fclose(fptr);
+*/
+    int N = 8;// atoi(argv[4]);
+    //int ctd_elem_cons = 100*exp;
 
     /**
       tabla e[1..N+1][0..N] que almacena los costos de cada subarbol i,j.
@@ -90,13 +89,20 @@ void main(int argc, char *argv[]); {
       Lo anterior se traduce en computar e[N+1,N] y e[1,0]
       Ocupa las celdas e[i,j] tales que j >= i-1
      */
-    float e[N+1][N+1];
+    float **e;
+    e = malloc((N+1) * sizeof(float *));
+    for (i = 0; i < (N+1); i++)
+        e[i] = malloc((N+1) * sizeof(float));
+
     /**
       Matriz para guardar la raíz de cada sub-arbol que contiene las llaves
       en el rango [i,j].
       Ocupa las celdas root[i,j] que cumplen la condición 1 < i =< j < N
      */
-    int raiz[N][N];
+    int **root;
+    root = malloc(N * sizeof(int *));
+    for (i = 0; i < N; i++)
+        root[i] = malloc(N * sizeof(int));
     /**
       Se necesita otra tabla por eficiencia.
       Se usa para evitar calcular los pesos w(i,j) desde cero cada vez que
@@ -105,12 +111,16 @@ void main(int argc, char *argv[]); {
       Para 1 <= i <= N+1 tenemos w[i,i-1] = q_(i-1)
       Para j >= i tenemos w[i,j] = w[i,j-1] + p_j + q_j
      */
-    float w[N+1][c+1];
+    float **w;
+    w = malloc((N+1) * sizeof(float *));
+    for (i = 0; i < (N+1); i++)
+        w[i] = malloc((N+1) * sizeof(float));
 
+    int llave[] = {1, 2, 3, 4, 5, 6, 7, 8};
     float p[] = {0.1, 0.025, 0.2, 0.125, 0.125, 0.05, 0.3, 0.075};
-    float q[N];
+    float q[N+1];
     // se inicializa a cero dado que estos datos nunca son consultados.
-    for(i=0;i<N;i++) {
+    for(i=0;i<N+1;i++) {
         q[i] = 0;
     }
 
@@ -123,31 +133,17 @@ void main(int argc, char *argv[]); {
 
     // en este punto ya están construidas las matrices e, w y raiz
     // ahora formamos el árbol.
-    Nodo *nuevo_nodo, *raiz;
-    raiz = NULL;
-
-    int izq = 1, der = N-1;
-    int raiz = -1;
-    for(i=0;i<1;i++) {
-        // obtenemos la raiz 
-        if (raiz == -1) {
-            raiz = root[izq,der];
-        } else {
-            raiz
-        }
-
-        // busco la raíz del 
-        nuevo_nodo = crear_nodo(1);
-        insertar(&raiz, &nuevo_nodo);
-    }
+    //crear_arbol_optimo(llaves, root, &raiz, 0, N-1);
+    raiz = crear_arbol_optimo(llave, root, 0, N-1);
 
     // una vez armado el árbol se imprime en pantalla
     dibujar_arbol(raiz);
 
     // ahora realizamos las búsquedas de cada llave
+    /*
     for(i=0;i<1;i++) {
-        buscar(&raiz, key);
-    }
+        buscar(&raiz, 1);
+    }*/
 }
 
 /**
@@ -162,10 +158,11 @@ Nodo *crear_nodo(int clave) {
     temp->padre = NULL;
     return temp;
 }
+
 /**
   Función que genera la matriz e, w y root.
  */
-void generar_tablas(float p[], float q[], int n, float e[n+1][n+1], float w[n+1][n+1], float root[n][n]) {
+void generar_tablas(float *p, float *q, int n, float **e, float **w, int **root) {
     int i,l,j,r;
     float t;
 
@@ -179,22 +176,50 @@ void generar_tablas(float p[], float q[], int n, float e[n+1][n+1], float w[n+1]
             j = i + l+1;
             e[i][j] = 1000;
             w[i][j] = w[i][j-1] + p[j-1] + q[j];
-
-            for(r=i;r<=j;r++) {
+            for(r=i;r<j;r++) {
                 t = e[i][r] + e[r+1][j] + w[i][j];
-
                 if (t < e[i][j]) {
                     e[i][j] = t;
-                    root[i][j-1] = r+1;
+                    root[i][j-1] = r;
                 }
             }
         }
     }
-    imprimir_matrices();
+    imprimir_matrices(e, w, root, n);
 }
 
-void crear_arbol() {
+/**
+  Construye un árbol a partir de la matriz root
+ */
+Nodo* crear_arbol_optimo(int *val, int **root, int izq, int der) {
+    Nodo *nuevo_nodo;
+    int indice, indice_izq, indice_der;
+
+    indice = root[izq][der];
+    nuevo_nodo = crear_nodo(val[indice]);
+
+    printf("Ejecutando rango [%d,%d], val[%d]= %d\n", izq, der, root[izq][der], val[indice]);
+    
+    indice_izq = indice;
+    indice_izq--;
+
+    indice_der = indice;
+    indice_der++;
+
+    if (!(izq > indice_izq)) {
+        // crear sub-árbol izquierdo 
+        nuevo_nodo->hijo_izq = crear_arbol_optimo(val, root, izq, indice_izq);
+        nuevo_nodo->hijo_izq-> padre = nuevo_nodo;
+    }
+    if (!(der < indice_der)) {
+        // crear sub-árbol derecho
+        nuevo_nodo->hijo_der = crear_arbol_optimo(val, root, indice_der, der);
+        nuevo_nodo->hijo_der-> padre = nuevo_nodo;
+    }
+
+    return nuevo_nodo;
 }
+
 /**
   Inserta un nodo en la estructura.
  */
@@ -206,12 +231,12 @@ void insertar(Nodo **raiz, Nodo **z) {
         y = x;
         // si la clave a insertar es menor que la que está en x.
         if ((*z)->clave < x->clave) {
-            (DEBUG?printf("\nLa clave ingresada es menor que %d, nos movemos al hijo izquierdo.", x->clave):1);
+            (DEBUG?printf("\nLa clave ingresada(%d) es menor que %d, nos movemos al hijo izquierdo.", (*z)->clave, x->clave):1);
             // nos movemos al nodo de la izquierda
             x = x->hijo_izq;
             // de lo contrario nos vamos a la derecha.
         } else {
-            (DEBUG?printf("\nLa clave ingresada es mayor que %d, nos movemos al hijo derecho.", x->clave):1);
+            (DEBUG?printf("\nLa clave ingresada(%d) es mayor que %d, nos movemos al hijo derecho.", (*z)->clave,x->clave):1);
             x = x->hijo_der;
         }
     }
@@ -300,6 +325,7 @@ void dibujar_arbol(Nodo *raiz) {
     dibujar_arbol_helper(raiz, 0);
     puts("");
 }
+
 /**
   Dibuja la estructura del árbol de izquierda a derecha.
 Ej:
@@ -350,20 +376,23 @@ void mostrar_nodo(Nodo *nodo) {
   Función que imprime en la salida estándar el contenido de las
   matrices e, w, raiz
  */
-void imprimir_matrices() {
+void imprimir_matrices(float **e, float **w, int **root, int N) {
     int row, columns;
+
     printf("\nMatriz e\n");
     for (row=0; row<N+1; row++) {
         for(columns=0; columns<N+1; columns++)
             printf("%f ", e[row][columns]);
         printf("\n");
     }
+
     printf("\nMatriz raiz\n");
     for (row=0; row<N; row++) {
         for(columns=0; columns<N; columns++)
-            printf("%d ", raiz[row][columns]);
+            printf("%d ", root[row][columns]);
         printf("\n");
     }
+
     printf("\nMatriz w\n");
     for (row=0; row<N+1; row++) {
         for(columns=0; columns<N+1; columns++)
